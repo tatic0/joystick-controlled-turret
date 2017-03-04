17eb7b37a32a9a0b1bc46ad85b6ca74c702e3add
tatico <tatico@gmail.com>
2017-03-04 18:15:21 +0100

    yay!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<Arduino.h>
-#include <Servo.h>
+#include<Servo.h>
+#include<math.h>
 
 Servo servo1;
 Servo servo2;
@@ -8,6 +9,7 @@ int analogX = 0;
 int analogY = 1; 
 int valX = 0;
 int valY = 0;
+
 
 void setup (){
   servo1.attach(9);
@@ -19,13 +21,15 @@ void setup (){
 void loop (){
   valX = analogRead(analogX);
   Serial.println(valX);
-  valX = map(valX, 0, 1023, 0, 180); //magic function to map values
+  valX = map(valX, 0, 1023, 180, 0); //magic function to map values
   Serial.println(valX);              //https://www.arduino.cc/en/Reference/Map
   servo1.write(valX);
+  delay(50);
   valY = analogRead(analogY);
   Serial.println(valY);
-  valY = map(valY, 0, 1023, 0, 180);
+  valY = map(valY, 0, 1023, 180, 0);
   Serial.println(valY);
   servo2.write(valY);
   delay(50);
 }
+
